878c6748e5b0e903cd3a878dc3f2a86b3150ed7c
Leandro Corneo <leandrocorneo@hotmail.com>
2025-10-29 23:05:49 -0300

    chore(app): register UsersModule and AuthModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as path from 'path';
 import { DatabaseModule } from './config/database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { DatabaseModule } from './config/database/database.module';
       envFilePath: path.resolve(__dirname, '../.env'),
     }),
     DatabaseModule,
+    UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
