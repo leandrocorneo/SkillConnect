53b45fab7b1d24fa5bf6c9d8e07acab36b364c14
Leandro Corneo <leandrocorneo@hotmail.com>
2026-01-11 14:14:15 -0300

    chore: hash util minor changes

--- backend/src/shared/utils/hash.util.ts
+++ backend/src/shared/utils/hash.util.ts
@@ -3,12 +3,12 @@ import * as bcrypt from 'bcrypt';
 export class HashUtil {
   static readonly saltRounds = 10;
 
-  static async hash(password: string): Promise<string> {
+  static async hash(data: string): Promise<string> {
     const salt = await bcrypt.genSalt(this.saltRounds);
-    return await bcrypt.hash(password, salt);
+    return await bcrypt.hash(data, salt);
   }
 
-  static async compare(password: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(password, hash);
+  static async compare(data: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(data, hash);
   }
 }
